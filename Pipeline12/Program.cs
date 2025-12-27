941da45847c23b76a8e71a6ee63350daf1b85ca8
snehalpokharkar96-dev <snehalpokharkar96@gmail.com>
2025-12-27 12:41:16 +0530

    Add .gitignore.txt
    Add .gitignore.txt

---
+++ Pipeline12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipeline12
+{
+    internal class Program
+    {
+        public void sub()
+        {
+            int a = 50, b = 20,c;
+            c = a - b;
+            Console.WriteLine("sub=" + c);
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.sub();
+            Console.ReadLine();
+        }
+    }
+}
